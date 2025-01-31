71a35e9dc60a6b3d073df1da0845655d958f2542
Hiruni Jayawardena <hiruniejayawardena@gmail.com>
2025-01-31 23:50:18 +0530

    navigation added

--- index.js
+++
@@ -1,6 +0,0 @@
-// Optional: Simple form submission feedback
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-  });
-  
